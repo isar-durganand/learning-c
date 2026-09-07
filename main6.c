ed0bb18d5d56c826dfb2f958155bc49e5b667c92
isar-durganand <isardurganand@gmail.com>
2026-09-07 09:36:03 +0530

    Odd or Even problem

--- main6.c
+++ main6.c
@@ -1,2 +1,18 @@
 //WAP to print, if the given number is Odd or Even
 
+#include <stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n%2==0){
+        printf("%d is an even number", n);
+    }
+    else{
+        printf("%d is an odd number", n);
+    }
+    return 0;
+}
